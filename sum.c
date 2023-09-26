2b77415a1ad6438778a669fc0731dcc4dbc239a9
zinal undhad <admin4@admin4s-MacBook-Air.local>
2023-09-26 11:52:40 +0530

    add new sum file

---
+++ sum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+int i;
+    int mynumber[5] = {12, 46, 38, 290, 47}; // Array indexes start with 0: [0] is the first element.
+                                            // [1] is the second element
+    // printf("%d\n", mynumber[0]);
+for (i=0; i<=4; i++)
+{
+// printf("%d\n",mynumber[i]);
+printf("%p\n", &mynumber[i]);
+}
+printf("%lu",sizeof(mynumber));
+// int myNumbers[4] = {25, 50, 75, 100};
+// int i;
+
+// for (i = 0; i < 4; i++)
+// {
+//     printf("%p\n", &myNumbers[i]);
+// }
+    return 0;
+}
+
+
+
+
+     
+
+
