e18cacccbfac2165d92914f14f56d6d7881f24ed
zinal undhad <admin4@admin4s-MacBook-Air.local>
2023-09-26 14:45:18 +0530

    add new c file

--- sum.c
+++ sum.c
@@ -2,29 +2,22 @@
 
 int main()
 {
-int i;
-    int mynumber[6] = {12, 46, 38, 290, 47,8,54}; // Array indexes start with 0: [0] is the first element.
-                                            // [1] is the second element
+    int i;
+    int mynumber[6] = {12, 46, 38, 290, 47, 8, 54}; // Array indexes start with 0: [0] is the first element.
+                                                    // [1] is the second element
     // printf("%d\n", mynumber[0]);
-for (i=0; i<=4; i++)
-{
-// printf("%d\n",mynumber[i]);
-printf("%p\n", &mynumber[i]);
-}
-printf("%lu",sizeof(mynumber));
-// int myNumbers[4] = {25, 50, 75, 100};
-// int i;
+    for (i = 0; i <= 4; i++)
+    {
+        // printf("%d\n",mynumber[i]);
+        printf("%p\n", &mynumber[i]);
+    }
+    printf("%lu", sizeof(mynumber));
+    // int myNumbers[4] = {25, 50, 75, 100};
+    // int i;
 
-// for (i = 0; i < 4; i++)
-// {
-//     printf("%p\n", &myNumbers[i]);
-// }
+    // for (i = 0; i < 4; i++)
+    // {
+    //     printf("%p\n", &myNumbers[i]);
+    // }
     return 0;
 }
-
-
-
-
-     
-
-
